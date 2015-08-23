ea3262b22c5dd1f729137bfbfcfb33176098032a
Marcus Kruse <kruse@vivai.de>
2015-08-23 09:32:40 +0200

    jsz.Observer documentation

--- public/javascripts/lib/jsz/Observer.js
+++ public/javascripts/lib/jsz/Observer.js
@@ -1,17 +1,43 @@
 /**
- * TODO: Documentation
+ * This script contains teh jsz.Observer.
+ * @version: 0.1.0
  */
 script({
   name: 'lib.jsz.Observer'
 }, function() {
   'use strict';
 
+  /**
+   * The jsz.Observer monitored get and set operation on an object.
+   */
   namespace('jsz').class('Observer').def({
 
     _path_: JSZ.EMPTY_STRING,
     _callback_: {},
     _data_: {},
 
+    /**
+     * The constructor for the jsz.Observer. The observation can be internal or
+     * external. If the constructor get just a config then the observation is
+     * internal and the created object contains the data. In this case the value
+     * config.data is mandatory. If the constructor get an object and a config
+     * then the observation is external. In this case is the value config.data
+     * optional.
+     * Plain objects will become nested observer objects and other objects will
+     * become values of the observer.
+     *
+     * @param {Object} [object] - The object to monitor.
+     * @param {Object} config - The observer configuration.
+     * @param {Object} config.callback
+     * @param {Function} config.callback.setter - This callback will be called
+     *  when a property of the observed object was set.
+     * @param {Function} config.callback.getter - This callback will be called
+     *  when a property of the observed object was get.
+     * @param {Object} config.callback.scope - The scope for the setter and
+     *  getter callbacks.
+     * @param {Object} [config.data] - Initial data for the monitored object.
+     * @constructor
+     */
     Observer: function(object, config) {
       if (arguments.length === 1) {
         // internal: The observer holds the data
@@ -28,17 +54,18 @@ script({
         path: JSZ.EMPTY_STRING
       });
 
+      // Save the path for nested observer objects.
       this._path_ = config.path === JSZ.EMPTY_STRING ?
         config.path : config.path + '.';
 
       this._data_ = {
-        current: {},
-        snapshot: {}
+        current: {}, // to save the current values
+        snapshot: {} // to save the initial values and or snapshots
       };
 
-      // prepare data
+      // Prepare data that will be observed.
       Object.keys(config.data).forEach(function(key) {
-        if (jsz.isObject(config.data[key])) {
+        if (jsz.isPlainObject(config.data[key])) {
           config.data[key] = new jsz.Observer({
             data: config.data[key],
             callback: config.callback,
@@ -57,23 +84,43 @@ script({
         getter: unite(config.callback.getter, config.callback.scope)
       };
 
+      // Define the properties for the observed object.
       jsz.Observer.defineProperties(this, object, config.data);
 
+      // Freeze the object to prevent the object for extension.
       Object.freeze(object);
-
     },
 
+    /**
+     * The setter to observe set operation of the observed object.
+     *
+     * @param key
+     * @param value
+     * @private
+     */
     _setter_: function(key, value) {
       this._callback_.setter(this._path_ + key, value);
       this._data_.current[key] = value;
     },
 
+    /**
+     * The getter to observe get operation of the observed object.
+     *
+     * @param key
+     * @returns {*}
+     * @private
+     */
     _getter_: function(key) {
       var value = this._data_.current[key];
       this._callback_.getter(this._path_ + key, value);
       return value;
     },
 
+    /**
+     * Returns true if the object was not changed after observation was started.
+     *
+     * @returns {boolean}
+     */
     hasNoChanges: function() {
       var current = this._data_.current;
       var snapshot = this._data_.snapshot;
@@ -92,6 +139,11 @@ script({
       });
     },
 
+    /**
+     * Returns true if the object was changed after observation was started.
+     *
+     * @returns {boolean}
+     */
     hasChanges: function() {
       return !this.hasNoChanges();
     }
@@ -99,10 +151,13 @@ script({
   });
 
   /**
+   * Defines all properties for the observed object an links the getters and
+   * setters to the observer object.
    *
    * @param {jsz.Observer} observer
    * @param {Object} object
    * @param {Object} properties
+   * @static
    */
   jsz.Observer.defineProperties = function(observer, object, properties) {
     Object.keys(properties).forEach(function(key) {
@@ -115,12 +170,28 @@ script({
     });
   };
 
+  /**
+   * Returns the handler for the get observation.
+   *
+   * @param observer
+   * @param key
+   * @returns {Function}
+   * @static
+   */
   jsz.Observer.getter = function(observer, key) {
     return function() {
       return observer._getter_(key);
     };
   };
 
+  /**
+   * Returns the handler for the set observation.
+   *
+   * @param observer
+   * @param key
+   * @returns {Function}
+   * @static
+   */
   jsz.Observer.setter = function(observer, key) {
     return function(value) {
       observer._setter_(key, value);
